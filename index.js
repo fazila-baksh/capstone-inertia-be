8332845bdf568d2f1474ae20dd44e2cf3eb42e1e
Fazila Baksh <fazilabak@gmail.com>
2025-01-15 20:51:58 -0500

    install cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 import userRoutes from "./routes/user-routes.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
+
+app.use(cors());
 
 // all users routes
 app.use("/user", userRoutes);
