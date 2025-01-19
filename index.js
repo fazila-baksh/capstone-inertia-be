7b344e03a24afd91cee2d923ae15bb9dd913d60f
Fazila Baksh <fazilabak@gmail.com>
2025-01-19 15:19:14 -0500

    updated user routes, and added more seed data

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 // all users routes
 app.use("/user", userRoutes);
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -5,22 +5,181 @@ import express from "express";
 import axios from "axios";
 const router = express.Router();
 
-router.post("/add", async (req, res) => {
+router.post("/:userId/habits/add", async (req, res) => {
+  const userId = req.params.userId;
+  const { habit_name, description, goal_frequency } = req.body;
+
   try {
-    const { habit_name, description, goal_frequency } = req.body;
+    if (!habit_name || !description || !goal_frequency) {
+      return res.status(400).json({
+        message: `Request body has missing properties`,
+      });
+    }
 
-    const newHabit = await knex("habits")
-      .insert({
-        habit_name,
-        description,
-        goal_frequency,
-      })
-      .returning("*");
+    const userIdExists = await knex("users").where("user_id", userId).first();
+    if (!userIdExists) {
+      return res.status(404).json({
+        message: `User with user ID ${userId} not found`,
+      });
+    }
 
-    res.status(201).json(newHabit[0]);
+    if (typeof goal_frequency !== "number") {
+      return res.status(404).json({
+        message: `Goal frequency is not a number`,
+      });
+    }
+
+    const data = {
+      habit_name: habit_name,
+      description: description,
+      goal_frequency: goal_frequency,
+      user_id: userId,
+    };
+
+    const result = await knex("habits").insert(data);
+    const newHabitId = result[0];
+    const newHabitItem = await knex("habits").where({
+      habit_id: newHabitId,
+    });
+
+    res.status(201).json(newHabitItem);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Error creating habit" });
+    res.status(500).json({
+      message: `Error creating new habit`,
+    });
+  }
+});
+
+router.put("/:userId/habits/:habitId/edit", async (req, res) => {
+  const habitId = req.params.habitId;
+  const { habit_name, description, goal_frequency } = req.body;
+
+  try {
+    if (!habit_name || !description || !goal_frequency) {
+      return res.status(400).json({
+        message: `Request body has missing properties`,
+      });
+    }
+
+    const habitIdExists = await knex("habits")
+      .where("habit_id", habitId)
+      .first();
+    if (!habitIdExists) {
+      return res.status(404).json({
+        message: `Habit with habit ID ${habitId} not found`,
+      });
+    }
+
+    if (typeof goal_frequency !== "number") {
+      return res.status(404).json({
+        message: `Goal frequency is not a number`,
+      });
+    }
+
+    const result = await knex("habits")
+      .where({ habit_id: habitId })
+      .update(req.body);
+
+    const updatedHabit = await knex("habits").where({ habit_id: habitId });
+
+    res.status(200).json(updatedHabit);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: `Error updating habit`,
+    });
+  }
+});
+
+router.delete("/:userId/habits/:habitId", async (req, res) => {
+  const habitId = req.params.habitId;
+
+  try {
+    const deleteHabit = await knex("habits").where({ habit_id: habitId }).del();
+
+    if (deleteHabit === 0) {
+      return res.status(404).json({
+        message: `Habit with habit ID ${habitId} not found`,
+      });
+    }
+
+    res.status(200).json({ message: "Habit deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: `Error deleting habit`,
+    });
+  }
+});
+
+router.delete("/:userId/logs/:logId", async (req, res) => {
+  const logId = req.params.logId;
+
+  try {
+    const deleteLog = await knex("habit_logs").where({ log_id: logId }).del();
+
+    if (deleteLog === 0) {
+      return res.status(404).json({
+        message: `Log with log ID ${logId} not found`,
+      });
+    }
+
+    res.status(200).json({ message: "Log deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: `Error deleting log`,
+    });
+  }
+});
+
+router.post("/:userId/habits/:habitId", async (req, res) => {
+  const habitId = req.params.habitId;
+  const { log_date, completed, log_month } = req.body;
+
+  try {
+    if (!log_date || !completed || !log_month) {
+      return res.status(400).json({
+        message: `Request body has missing properties`,
+      });
+    }
+
+    const habitIdExists = await knex("habits")
+      .where("habit_id", habitId)
+      .first();
+
+    if (!habitIdExists) {
+      return res.status(404).json({
+        message: `Habit with habit ID ${habitId} not found`,
+      });
+    }
+
+    if (typeof completed !== "boolean") {
+      return res.status(404).json({
+        message: `Completed is not a boolean`,
+      });
+    }
+
+    const data = {
+      log_date: log_date,
+      log_month: log_month,
+      completed: completed,
+      habit_id: habitId,
+    };
+
+    const result = await knex("habit_logs").insert(data);
+    const newLogId = result[0];
+    const newLogItem = await knex("habit_logs").where({
+      log_id: newLogId,
+    });
+
+    res.status(201).json(newLogItem);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: `Error creating new log`,
+    });
   }
 });
 
@@ -56,9 +215,25 @@ router.get("/:userId", async (req, res) => {
 router.get("/:userId/habits/daily", async (req, res) => {
   try {
     const userId = req.params.userId;
-    const data = await knex("habits")
+    const currDate = new Date().toLocaleDateString("en-CA");
+
+    const habits = await knex("habits")
       .where({ user_id: userId })
       .select("habit_id", "habit_name");
+
+    const data = await Promise.all(
+      habits.map(async (habit) => ({
+        habit_id: habit.habit_id,
+        habit_name: habit.habit_name,
+        log_id:
+          (
+            await knex("habit_logs")
+              .where({ habit_id: habit.habit_id, log_date: currDate })
+              .select("log_id")
+              .first()
+          )?.log_id || 0,
+      }))
+    );
 
     res.status(200).json(data);
   } catch (error) {
@@ -72,7 +247,7 @@ router.get("/:userId/habits/weekly", async (req, res) => {
     const userId = req.params.userId;
 
     // Code to get the start and end of the week
-    const curr = new Date("2025-01-06T01:49:11.335Z"); // get current date
+    const curr = new Date(); // get current date
     const first = curr.getDate() - curr.getDay(); // first day = day of month - day of week
     const last = first + 6;
     const startOfWeek = new Date(curr.setDate(first)).toLocaleDateString(
@@ -104,32 +279,51 @@ router.get("/:userId/habits/weekly", async (req, res) => {
 });
 
 router.get("/:userId/habits/:habitId", async (req, res) => {
+  const habitId = req.params.habitId;
   try {
-    const userId = req.params.userId;
-    const habitId = req.params.habitId;
     const habit = await knex("habits")
-      .where({ habit_id: habitId, user_id: userId })
+      .where({ habit_id: habitId })
       .select(
+        "habits.habit_id",
         "habits.habit_name",
         "habits.description",
         "habits.goal_frequency"
       );
 
-    if (!habit) {
-      return res.status(404).send("Habit not found");
-    }
+    const data = await knex("habit_logs")
+      .where({ habit_id: habitId })
+      .select(
+        "habit_logs.log_month",
+        "habit_logs.log_date",
+        "habit_logs.completed"
+      );
 
-    // const data = await Promise.all({
-    //   habit_id: habit.habit_id,
-    //   habit_name: habit.habit_name,
-    //   description: habit.description,
-    //   dailyTracking: await knex("habit_logs")
-    //     .where({ habit_id: habit.habit_id })
-    //     .whereBetween("log_date", [startOfWeek, endOfWeek])
-    //     .select("log_date", "completed"),
-    // });
+    const monthObjs = [
+      { January: 0 },
+      { February: 0 },
+      { March: 0 },
+      { April: 0 },
+      { May: 0 },
+      { June: 0 },
+      { July: 0 },
+      { August: 0 },
+      { September: 0 },
+      { October: 0 },
+      { November: 0 },
+      { December: 0 },
+    ];
 
-    res.status(200).json(habit);
+    const graphYValues = await Promise.all(
+      monthObjs.map((monthObj) => {
+        const monthName = Object.keys(monthObj)[0];
+        const count = data.filter((log) => log.log_month === monthName).length;
+        return count;
+      })
+    );
+
+    const updatedData = { ...habit[0], graphYValues: graphYValues };
+
+    res.status(200).json(updatedData);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
